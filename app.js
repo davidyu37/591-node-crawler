bffba65519fb1c5ef82bb91e066a20642ba95c0c
David <davidyu37@gmail.com>
2016-12-17 23:02:42 +0800

    gmail

--- app.js
+++ app.js
@@ -17,16 +17,24 @@ var async = require('async');
 // var users = require('./routes/user');
 
 //For nodemailer
+// var connection = {
+//     host: 'smtp.mailgun.org',
+//     secure: true,
+//     auth: {
+//         user: 'postmaster@sandbox71cee6567e504a01a0c648f273897726.mailgun.org',
+//         pass: '1234qwer'
+//     },
+//     logger: true
+// };
 var connection = {
-    host: 'smtp.mailgun.org',
+    host: 'smtp.gmail.com',
     secure: true,
     auth: {
-        user: 'postmaster@sandbox71cee6567e504a01a0c648f273897726.mailgun.org',
-        pass: '1234qwer'
+        user: 'davidyu37@gmail.com',
+        pass: 'basketballmvp1'
     },
     logger: true
 };
-
 
 // create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport(connection);
@@ -81,33 +89,59 @@ if (app.get('env') === 'development') {
     });
 }
 
-cron.schedule('*/59 * * * *', function(){
-    console.log('running a task every one hour');
-    request.get({
-        url: 'https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&region=1&order=posttime&orderType=desc&section=3,5,7,1&pattern=2&hasimg=1&rentprice=4&other=lease',                                                              
-    }, function(err, res591, body) {
-        if(body) {
-            var content = JSON.parse(body);
-            var newList = content.data.data;
-            var jsonNewList = JSON.stringify(newList);
+// cron.schedule('*/59 * * * *', function(){
+//     console.log('running a task every one hour');
+//     request.get({
+//         url: 'https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&region=1&order=posttime&orderType=desc&section=3,5,7,1&pattern=2&hasimg=1&rentprice=4&other=lease',                                                              
+//     }, function(err, res591, body) {
+//         if(body) {
+//             var content = JSON.parse(body);
+//             var newList = content.data.data;
+//             var jsonNewList = JSON.stringify(newList);
             
-            if(data[0].post_id === newList[0].post_id) {
-                console.log('nothing changed');
-                //Do nothing because there's no new house posted'
+//             if(data[0].post_id === newList[0].post_id) {
+//                 console.log('nothing changed');
+//                 //Do nothing because there's no new house posted'
+//             } else {
+//                 fs.writeFile('house_data.json', jsonNewList, function(err) {
+//                 if (err) {
+//                     console.log('Something when wrong');
+//                 } else {
+//                     console.log('Saved!');
+//                     // constructHtml(newList);
+//                     //Send email notification
+//                     sendMail(newList);
+//                 }
+//                 });
+//             }
+//         }
+//     });
+// });
+
+request.get({
+    url: 'https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&region=1&order=posttime&orderType=desc&section=3,5,7,1&pattern=2&hasimg=1&rentprice=4&other=lease',                                                              
+}, function(err, res591, body) {
+    if(body) {
+        var content = JSON.parse(body);
+        var newList = content.data.data;
+        var jsonNewList = JSON.stringify(newList);
+        
+        if(data[0].post_id === newList[0].post_id) {
+            console.log('nothing changed');
+            // sendMail(newList);
+            //Do nothing because there's no new house posted'
+        } else {
+            fs.writeFile('house_data.json', jsonNewList, function(err) {
+            if (err) {
+                console.log('Something when wrong');
             } else {
-                fs.writeFile('house_data.json', jsonNewList, function(err) {
-                if (err) {
-                    console.log('Something when wrong');
-                } else {
-                    console.log('Saved!');
-                    // constructHtml(newList);
-                    //Send email notification
-                    sendMail(newList);
-                }
-                });
+                console.log('Saved!');
+                //Send email notification
+                sendMail(newList);
             }
+            });
         }
-    });
+    }
 });
 
 
@@ -116,27 +150,34 @@ function sendMail(obj) {
     var html = '';
     if(obj) {
         var eachHtml;
-        obj.forEach(function(house) {
-            eachHtml = '<div><a href="https://rent.591.com.tw/rent-detail-' + 
-            house.post_id + '.html' + '"><img src="' + 
-            house.cover + '"/></a><p>$ ' + 
-            house.price + '</p><p>' + 
-            house.section_name + '</p><p>' +
-            house.fulladdress + '</p><p>' + 
-            house.layout + '</p><p>' + 
-            house.floorInfo + '</p><p>' + 
-            house.living + '</p><p>' + 
-            house.condition + '</p></div><hr/>';
-            html += eachHtml;
-        });
+        var text = '<img src="'+ obj[0].cover + '" /><br/>';
+        text += '<p>' + obj[0].fulladdress + '</p><br/>';
+        text += '<p>$' + obj[0].price + '</p><br/>';
+        text += '<p>' + obj[0].section_name + '</p><br/>';
+        text += 'https://rent.591.com.tw/rent-detail-' + obj[0].post_id + '.html';
+        text += '<br/> View recent list: https://crawling591.herokuapp.com/';
+        // obj.forEach(function(house) {
+        //     eachHtml = '<div><a href="https://rent.591.com.tw/rent-detail-' + 
+        //     house.post_id + '.html' + '"><img src="' + 
+        //     house.cover + '"/></a><p>$ ' + 
+        //     house.price + '</p><p>' + 
+        //     house.section_name + '</p><p>' +
+        //     house.fulladdress + '</p><p>' + 
+        //     house.layout + '</p><p>' + 
+        //     house.floorInfo + '</p><p>' + 
+        //     house.living + '</p><p>' + 
+        //     house.condition + '</p></div><hr/>';
+        //     html += eachHtml;
+        // });
         console.log(html);
         // setup e-mail data with unicode symbols
+        // 	kaoxiaoyang@gmail.com
         var mailOptions = {
-            from: '"591 Houses🔑" <davidyu37@gmail.com>', // sender address
-            to: 'davidyu37@gmail.com', // list of receivers
-            subject: '591 Houses🔑', // Subject line
-            text: 'Check it out', // plaintext body
-            html: html // html body
+            from: '"591 Houses🔑" <davidyu37@gmail.com>',
+            to: 'davidyu37@gmail.com, kaoxiaoyang@gmail.com',
+            subject: '591 Houses🔑',
+            // text: text,
+            html: text // html body
         };
 
         // send mail with defined transport object
